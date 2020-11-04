b884d67accf2b67858c9d12e943dd17bc334fe30
NBJJ <73909448+NBJJ@users.noreply.github.com>
2020-11-04 16:15:37 -0300

    Update Cabina.java

--- Cabina.java
+++ Cabina.java
@@ -1,20 +1,17 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-
-
+//MAIN DEL PROYECTO
 
 public class Cabina {
-	
 	public static void main(String arg[]){
 	
-		
+		//Creamos un objeto peaje para enviar los parámetros iniciales 
 		Peaje peaje1 = new Peaje();
-		
+									//enviamos como parámetro la dirrecion del archivo requerido 
 		ArrayList<Peaje> ListaTodos =  peaje1.CapturaTXT("C:\\camion1.txt");
-		int opc = 0; 
+		
+		int opc=0; 
+		//Enviamos la lista tipo Peaje para ser usada y llenada 
 		peaje1.toCamionTurno(opc, ListaTodos);
 		
 		
